1150fd2f47dea53f83939c383484567a511b1469
Awesome Inc <awesomeinc@AwesomeIsaacMBP.localdomain>
2018-10-05 12:39:11 -0400

    Applied styles, added js to print menu on cards, updated README.md

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let apiRequest = new XMLHttpRequest();
+
+document.onreadystatechange = function () {
+  if (document.readyState == "interactive") {
+    window.onload = getMenu;
+  }
+};
+
+function getMenu() {
+  let url = 'https://entree-f18.herokuapp.com/v1/menu';
+  apiRequest.onload = onSuccess; // apiRequest.onerror = onError;
+
+  apiRequest.open('get', url, true);
+  apiRequest.send();
+}
+
+function onSuccess() {
+  let menu = JSON.parse(apiRequest.responseText).menu_items;
+  let menuLength = JSON.parse(apiRequest.responseText).menu_size;
+  let cards = [];
+
+  for (let i = 0; i < menuLength; i++) {
+    cards.push(document.getElementById('body' + i));
+
+    if (cards.length === menuLength) {
+      for (let j = 0; j < cards.length; j++) {
+        cards[j].innerHTML = menu[j].description;
+      }
+    }
+  }
+}
+
+function onError() {}
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+1. cd { project directory }
+
+2. git clone https://github.com/Cgmccarty/project2-restaurant.git
+
+3. npm install
